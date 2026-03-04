318998cd8e6c62c0b4cb89aced290e8ae5ac00fe
Dr1v4nSs <104269035+Dr1n4nSs@users.noreply.github.com>
2026-03-04 22:47:06 +0500

    Add files via upload

---
+++ ConsoleApp2/Program.fs
@@ -0,0 +1,41 @@
+﻿open System
+
+let tryParseBinary (input: string) =
+    try
+        let value = Convert.ToInt32(input, 2)
+        if value >= 1 && value <= 9 then
+            Some(value)
+        else
+            None
+    with _ -> None
+
+
+let rec getValidBinaryInput index total =
+    printf "\nВведите двоичное число 1-9 (элемент %d из %d): " index total
+    let input = Console.ReadLine()
+    match tryParseBinary input with
+    | Some(value) -> value
+    | None -> 
+        printfn "Введите корректное двоичное число от 1 до 9."
+        getValidBinaryInput index total
+
+[<EntryPoint>]
+let main _ =
+    printf "Введите количество элементов: "
+    match Int32.TryParse(Console.ReadLine()) with
+    | (true, n) when n > 0 ->
+        let binaryNumbers = 
+            Seq.init n (fun i -> getValidBinaryInput (i + 1) n)
+            |> Seq.cache
+
+        printfn "\nПоследовательность:"
+        binaryNumbers |> Seq.iter (printf "%d ")
+        printfn "\n" 
+
+        let totalSum = binaryNumbers |> Seq.fold (fun acc x -> acc + x) 0
+        
+        printfn "Итоговая сумма: %d" totalSum
+        
+    | _ -> printfn "Некорректное количество элементов."
+    
+    0
---
+++ ConsoleApp3/Program.fs
@@ -0,0 +1,34 @@
+﻿open System
+open System.IO
+
+let rec findFile (fileName: string) (currentDir: string) =
+    seq {
+        let files = Directory.EnumerateFiles(currentDir)
+        let foundInCurrent = files |> Seq.filter (fun path -> Path.GetFileName(path) = fileName)
+        yield! foundInCurrent
+
+        let subDirs = Directory.EnumerateDirectories(currentDir)
+        for dir in subDirs do
+            yield! findFile fileName dir
+    }
+
+[<EntryPoint>]
+let main _ =
+    printf "Введите название файла (с расширением): "
+    let fileName = Console.ReadLine()
+    
+    printf "Введите путь к начальному каталогу : "
+    let startPath = Console.ReadLine()
+
+    if Directory.Exists(startPath) then
+        
+        let results = findFile fileName startPath
+        if Seq.isEmpty results then
+            printfn "Файл '%s' не найден." fileName
+        else
+            printfn "Файл(ы) найдены:"
+            results |> Seq.iter (printfn "- %s")
+    else
+        printfn "Указанный путь не существует."
+
+    0
